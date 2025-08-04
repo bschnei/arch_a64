564524a0c2d6daf58c06fc8b885267458104fdec
Benjamin Schneider <ben@bens.haus>
2025-08-04 13:46:00 -0700

    add repo push/pull scripts

---
+++ pkgrepos/pull.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo .)")" || exit; pwd)
+readonly script_dir
+
+repo=$1
+readonly repo
+
+if [ -z $repo ]; then
+  echo "no repo specified"
+  exit
+fi
+
+rsync -avh --delete "repo.bens.haus:/mnt/repo/arch/${repo}/os/aarch64/" "${script_dir}/${repo}/"
