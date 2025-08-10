6bddc1bdf5791c5cb7a1d354fb07eb89b69cccf2
Benjamin Schneider <ben@bens.haus>
2025-08-09 20:14:14 -0700

    sync remote repo on successful release

--- release.sh
+++ release.sh
@@ -3,7 +3,14 @@
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo .)")" || exit; pwd)
 readonly script_dir
 
-rsync -avh --delete --exclude staging.* --exclude testing.* "${script_dir}/pkgrepos/staging/" "${script_dir}/pkgrepos/testing/"
+pkgrepos_path="${script_dir}/pkgrepos/"
+from="staging"
+to="testing"
+readonly pkgrepos_path from to
 
-rm "${script_dir}"/pkgrepos/testing/testing.*
-repo-add "${script_dir}/pkgrepos/testing/testing.db.tar.gz" "${script_dir}"/pkgrepos/testing/*.pkg.*
+rsync -avh --delete --exclude ${to}.* --exclude ${from}.* "${pkgrepos_path}/${from}/" "${pkgrepos_path}/${to}/"
+
+rm "${pkgrepos_path}/${to}/${to}."*
+if ! repo-add "${script_dir}/pkgrepos/testing/testing.db.tar.gz" "${script_dir}"/pkgrepos/testing/*.pkg.*; then exit; fi
+
+bash "${script_dir}/pkgrepos/push.sh" "testing"
