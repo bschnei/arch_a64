ffe960a2ef39d2c324d70bc75d53c1f6b633affb
Benjamin Schneider <ben@bens.haus>
2025-11-20 10:00:58 -0800

    add script for dropping packages

---
+++ drop.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo .)")" || exit; pwd)
+readonly script_dir
+
+. "$(dirname "$(readlink -e "$0")")/functions"
+
+pkgrepo_path="${script_dir}/pkgrepos"
+srcrepos_path="${script_dir}/srcrepos"
+state_path="${script_dir}/state"
+readonly pkgrepo_path srcrepos_path
+
+pkgname="${1}"
+
+if [ -n "${2}" ]; then
+  pkgrepo="${2}"
+else
+  pkgrepo=$(get_pkgrepo "${pkgname}")
+fi
+
+repo-remove "${pkgrepo_path}/${pkgrepo}/${pkgrepo}.db.tar.gz" "${pkgname}"
+find "${pkgrepo_path}/${pkgrepo}/" -type f -regextype posix-extended -regex ".*/${pkgname}-[^-]+-[^-]+-[^-]+.pkg.tar.zst" -delete
+find "${pkgrepo_path}/${pkgrepo}/" -type f -regextype posix-extended -regex ".*/${pkgname}-[^-]+-[^-]+-[^-]+.pkg.tar.zst.sig" -delete
+
+rm -rf "${srcrepos_path}/${pkgname}"
+ 
+# remove from tobuild list 
+pattern=$(echo "${pkgname}" | sed 's/+/\\+/g')
+sed -i "/^${pattern}/d" "${state_path}/tobuild"
