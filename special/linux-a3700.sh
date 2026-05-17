ceb09f3a5c91ef9690c574939d7d80fe588cd3ee
Benjamin Schneider <ben@bens.haus>
2026-05-17 15:57:57 -0700

    rename aur to forge

--- special/linux-a3700.sh
+++ special/linux-a3700.sh
@@ -48,8 +48,8 @@ git push --tags
 
 # add to aur package repo
 for pkg in *.pkg.tar.*; do
-  mv "${pkg}" "${pkgrepos_path}/aur"
-  repo-add --remove "${pkgrepos_path}/aur/aur.db.tar.gz" "${pkgrepos_path}/aur/${pkg}"
+  mv "${pkg}" "${pkgrepos_path}/forge"
+  repo-add --remove "${pkgrepos_path}/forge/forge.db.tar.gz" "${pkgrepos_path}/forge/${pkg}"
 done
 
 # remove build artifacts
